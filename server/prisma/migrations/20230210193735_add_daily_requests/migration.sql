b59a1c98a67a9455c9d2e79370337cee4888614b
Meysam Shamsebagi <meysam.shm@outlook.com>
2023-02-10 23:11:03 +0330

    feat: add daily requests field

---
+++ server/prisma/migrations/20230210193735_add_daily_requests/migration.sql
@@ -0,0 +1,16 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "username" TEXT NOT NULL,
+    "password" TEXT,
+    "dailyRequests" INTEGER NOT NULL DEFAULT 30,
+    "remainingRequests" INTEGER NOT NULL DEFAULT 30,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+INSERT INTO "new_User" ("createdAt", "id", "password", "username") SELECT "createdAt", "id", "password", "username" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
