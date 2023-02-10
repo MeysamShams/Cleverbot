33dbef342ffbb5ba0ac0393b68f147b79c219597
Meysam Shamsebagi <meysam.shm@outlook.com>
2023-02-11 00:38:20 +0330

    feat: create user model

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -1,4 +1,17 @@
+import { Request } from "express"
+
 export interface UserAuth{
     status:number
     token:string
 }
+
+export interface User{
+    id:number
+    username:string
+    createdAt:Date
+    dailyRequests: number
+    remainingRequests: number
+}
+export interface UserRequest extends Request {
+    user:string
+}   
