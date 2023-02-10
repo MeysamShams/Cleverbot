1575c51a9805440951b4be766933fba0efd57eca
Meysam Shamsebagi <meysam.shm@outlook.com>
2023-02-10 23:35:05 +0330

    feat: create login service and controller

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,8 +1,12 @@
 import express,{Router,Request,Response} from 'express'
-import { createUser } from '../services/auth.service';
+import { createUser, login } from '../services/auth.service';
 const router:Router=express.Router();
 
 router.post("/register",async(req:Request,res:Response)=>{
     res.send(await createUser(req.body?.username,req.body?.password))
+})
+
+router.post("/login",async(req:Request,res:Response)=>{
+    res.send(await login(req.body?.username,req.body?.password))
 })
 export const authController=router;
--- server/src/services/auth.service.ts
+++ server/src/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Error } from '../models/error.model'
-import { hashPassword } from '../utils/bcrypt'
+import { comparePassword, hashPassword } from '../utils/bcrypt'
 import {prisma} from '../configs/config'
 import { UserAuth } from '../models/user.model'
 import { generateAccessToken } from '../utils/jwt'
@@ -25,5 +25,32 @@ export const createUser=async(username:string,password:string):Promise<UserAuth|
         }else{
             return {error:"Error on creating user!",status:503}
         }
+    }
+}
+
+
+export const login=async(username:string,password:string):Promise<UserAuth|Error|undefined>=>{
+    try{
+        const user=await prisma.user.findUnique({where:{username}})
+        const errorMessage:Error={
+            error:"Username or password is wrong!",
+            status:401
+        }
+
+        if(user){
+            if(await comparePassword(password,user.password as string)){
+                return {
+                    token:generateAccessToken({username,id:user?.id}) as string,
+                    status:200
+                }
+            }else{
+                return errorMessage
+            }
+        }else{
+            return errorMessage
+        }
+
+    }catch(err:any){
+        return {error:"An error occurred on checking credentials!",status:503}
     }
 }
