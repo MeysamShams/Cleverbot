f488b76cfc81056a4fcab2f1eac36951e016c0e1
Meysam Shamsebagi <meysam.shm@outlook.com>
2023-02-11 01:02:54 +0330

    feat: validate input data

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,12 +1,22 @@
 import express,{Router,Request,Response} from 'express'
 import { createUser, login } from '../services/auth.service';
+import { body, validationResult } from 'express-validator';
+
 const router:Router=express.Router();
 
-router.post("/register",async(req:Request,res:Response)=>{
+router.post("/register",body("username").isLength({min:4,max:50}),body("password").isLength({min:6}),async(req:Request,res:Response)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array(),status:400 });
+    }
     res.send(await createUser(req.body?.username,req.body?.password))
 })
 
-router.post("/login",async(req:Request,res:Response)=>{
+router.post("/login",body("username").isLength({min:4,max:50}),body("password").isLength({min:6}),async(req:Request,res:Response)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array(),status:400 });
+    }
     res.send(await login(req.body?.username,req.body?.password))
 })
 export const authController=router;
