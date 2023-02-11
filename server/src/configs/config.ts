93cd8d2343fa83fdd14f3254eb395997e9514942
Meysam Shamsebagi <meysam.shm@outlook.com>
2023-02-11 22:49:17 +0330

    feat: add openai configuration

--- server/src/configs/config.ts
+++ server/src/configs/config.ts
@@ -1,3 +1,21 @@
 import { PrismaClient } from "@prisma/client";
+import { Configuration, OpenAIApi } from "openai";
 
+// db configuration
 export const prisma=new PrismaClient()
+
+// open ai configuration
+const openaiConfig=new Configuration({
+    apiKey:process.env.OPENAI_TOKEN
+})
+export const oa={
+    openai:new OpenAIApi(openaiConfig),
+    modelConfiguration:{
+        model: "text-davinci-003",
+        temperature: 0,
+        max_tokens: 3000,
+        top_p: 1.0,
+        frequency_penalty: 0.5,
+        presence_penalty: 0.0,
+    }
+}
