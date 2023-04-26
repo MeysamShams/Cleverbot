29e6791f48d6d16f68cbd44e7d49d5ed35c87b38
Meysam Shams <meysam.shm@outlook.com>
2023-04-26 22:42:07 +0330

    fix: remove navbar outside of router provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
+import {RouterProvider} from 'react-router-dom'
+import { router } from "./routes/index.routes"
 function App() {
-  return <h1>
-    Hello World!
-  </h1>
+  return <>
+    <RouterProvider router={router} />
+  </>
 }
 
 export default App
