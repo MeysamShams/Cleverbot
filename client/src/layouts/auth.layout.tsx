4498f3eb2f898c1b3cb8cbe43e5652d867f5b95b
Meysam Shamsebagi <meysam.shm@outlook.com>
2023-04-27 19:35:03 +0330

    fix: fix navigation bug

--- client/src/layouts/auth.layout.tsx
+++ client/src/layouts/auth.layout.tsx
@@ -1,18 +1,13 @@
 import { Navbar } from "@/components/navbar/navbar.component"
 import { AuthContext } from "@/context/auth.context"
 import { Path } from "@/routes/path.routes"
-import { useContext, useEffect } from "react"
-import { Outlet, useNavigate } from "react-router-dom"
+import { useContext } from "react"
+import { Navigate, Outlet } from "react-router-dom"
 
 export const AuthLayout=()=>{
     const authCtx=useContext(AuthContext)
-    const navigator=useNavigate()
-    useEffect(()=>{
-        if(authCtx.isLoggedIn){
-            navigator(Path.User+Path.Chat,{replace:true,})
-        }
-    },[authCtx.isLoggedIn])
     return <>
+    {authCtx.isLoggedIn && <Navigate to={Path.User+"/"+Path.Chat} replace={true}/>}
     <Navbar/>
     <section className=" sm:w-full md:w-96 mx-2 md:mx-auto my-3 p-5">
         <Outlet/>
