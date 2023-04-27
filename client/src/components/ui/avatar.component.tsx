cbc5d56a8ee0e8f4d525165abb43430da4de2972
Meysam Shams <meysam.shm@outlook.com>
2023-04-27 13:35:02 +0330

    feat: add avatar component

---
+++ client/src/components/ui/avatar.component.tsx
@@ -0,0 +1,7 @@
+export const Avatar=(props:{name:string})=>{
+    return           <div className="avatar placeholder">
+    <div className="bg-neutral-focus text-neutral-content rounded-full w-10">
+      <span className="text-xl">{props.name.substring(0,1).toUpperCase()}</span>
+    </div>
+  </div> 
+}
