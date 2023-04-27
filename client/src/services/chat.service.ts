ebdc37e795dadac9c341855c6e49b3e19748774b
Meysam Shamsebagi <meysam.shm@outlook.com>
2023-04-28 00:58:56 +0330

    feat: create sendMessage method

--- client/src/services/chat.service.ts
+++ client/src/services/chat.service.ts
@@ -1,5 +1,10 @@
+import { ChatResponse } from '@/types/chat.type'
 import http from './http.service'
 
 export const getMessages=async(page=1)=>{
     return await http.get("/chat/messages",{params:{page}})
+}
+
+export const sendMessage=async(message:string):Promise<{data:ChatResponse}>=>{
+    return await http.post("/chat",{message})
 }
