a8484a220f02ab2beeef83bee97ffeda437ba123
Meysam Shamsebagi <meysam.shm@outlook.com>
2023-04-27 21:38:00 +0330

    feat: add data type

--- client/src/hooks/useFetch.hook.ts
+++ client/src/hooks/useFetch.hook.ts
@@ -1,15 +1,16 @@
 import { UseHttpInterface } from "@/types/http.type"
+import { AxiosResponse } from "axios"
 import { useEffect, useState } from "react"
 
-export const useFetch=(service:Promise<unknown>):UseHttpInterface=>{
+export const useFetch=<T>(service:Promise<any>):UseHttpInterface<T>=>{
     const [isLoading,setIsLoading]=useState(false)
     const [isError,setIsError]=useState(false)
-    const [data,setData]=useState<any>(null)
+    const [data,setData]=useState<T|undefined>()
     const getData=async()=>{
         try{
             setIsError(false)
             setIsLoading(true)
-            const data=await Promise.all([service]);
+            const data=await service;
             setIsLoading(false);
             setData(data)
     
--- client/src/types/http.type.ts
+++ client/src/types/http.type.ts
@@ -1,5 +1,12 @@
-export interface UseHttpInterface {
+export interface UseHttpInterface<T> {
     isLoading:boolean
     isError:boolean
-    data:any,
+    data:T|undefined,
+}
+
+export interface Pagination<T>{
+    total:number
+    page:number
+    itemPerPage:number,
+    results:T
 }
