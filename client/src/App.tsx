8f9304699375f863ce2bd1b41bb5eb4bdc742da2
Meysam Shams <meysam.shm@outlook.com>
2023-04-27 11:41:41 +0330

    feat: add Toaster container

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,22 @@
 import {RouterProvider} from 'react-router-dom'
 import { router } from "./routes/index.routes"
+import { Toaster } from 'react-hot-toast'
 function App() {
   return <>
+  				<Toaster
+					position="bottom-left"
+					toastOptions={{
+						style: {
+							background: '#29283d',
+							color: '#fff',
+							padding: '10px 20px',
+							fontSize: '15px',
+						},
+					}}
+					containerStyle={{
+						top: '10px',
+					}}
+				/>
     <RouterProvider router={router} />
   </>
 }
