36e61d2e050cbc271ba1d50208bfc4bb5ba7b682
Meysam Shamsebagi <meysam.shm@outlook.com>
2023-04-27 20:40:42 +0330

    fix: fix call service problem

--- client/src/hooks/useFetch.hook.ts
+++ client/src/hooks/useFetch.hook.ts
@@ -1,15 +1,15 @@
 import { UseHttpInterface } from "@/types/http.type"
 import { useEffect, useState } from "react"
 
-export const useFetch=(service:Function):UseHttpInterface=>{
+export const useFetch=(service:Promise<unknown>):UseHttpInterface=>{
     const [isLoading,setIsLoading]=useState(false)
     const [isError,setIsError]=useState(false)
-    const [data,setData]=useState(null)
+    const [data,setData]=useState<any>(null)
     const getData=async()=>{
         try{
             setIsError(false)
             setIsLoading(true)
-            const data=await service()
+            const data=await Promise.all([service]);
             setIsLoading(false);
             setData(data)
     
