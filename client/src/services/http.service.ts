dbe9c57b4b6bdd6218f9cdbeae2b9df795238aa8
Meysam Shams <meysam.shm@outlook.com>
2023-04-27 11:19:14 +0330

    feat: config axios config

---
+++ client/src/services/http.service.ts
@@ -0,0 +1,89 @@
+import axios, { InternalAxiosRequestConfig } from "axios";
+import { toast } from "react-hot-toast";
+
+// set jwt header
+const handleAuthorizationHeader = (config: InternalAxiosRequestConfig) => {
+
+    const usrtkn = localStorage.getItem("token");
+    let authorizationHeader;
+    usrtkn && typeof usrtkn == "string"
+      ? (authorizationHeader = `JWT ${usrtkn}`)
+      : (authorizationHeader = ``);
+    config.headers.Authorization = authorizationHeader;
+
+  return config;
+};
+
+// toast errors
+const handleErrors = (error: any): void => {
+  if (axios.isAxiosError(error)) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx      
+      const statusCode = error.response.status;
+      const errorMessage = extractErrorMessage(error.response.data);
+
+      if (statusCode >= 500) {
+        // redirect to 500 error page
+        toast.error("Server Error :(", { id: "500" });
+      } else if (statusCode >= 400 && statusCode < 500) {
+        // bad request
+        if (statusCode === 400) {
+          toast.error(errorMessage, { id: "400" });
+        }
+        // unauthorized
+        if (statusCode === 401) {
+          toast.error(errorMessage, { id: "401" });
+        }
+        // forbidden
+        if (statusCode === 403) {
+          toast.error("Forbidden!", { id: "403" });
+        }
+        // not found
+        if (statusCode === 404) {
+          // redirect to 404 error page
+          toast.error("Not found!", { id: "404" });
+        }
+        // conflict
+        if (statusCode === 409) {
+          toast.error(errorMessage, { id: "409" });
+        }
+      } else {
+        toast.error("Sorry, an error has occurred :(", { id: "unkown" });
+      }
+    } else if (error.request) {
+      toast.error("Network Error !", {
+        id: "networkError",
+      });
+    } else {
+      toast.error("Sorry, an error has occurred :(", { id: "unkown" });
+    }
+  } else {
+    toast.error("Sorry, an error has occurred :(", { id: "unkown" });
+  }
+  throw new Error(error)
+};
+
+const extractErrorMessage = (errors: any): string => {
+  let errorMessage = "";
+  if (errors && typeof errors==="object") {
+    Object.keys(errors).forEach((msg: string) => (errorMessage += errors[msg] + "\n"));
+  } else if (typeof errors === "string") errorMessage = errors;
+  else errorMessage = "Sorry, an error has occurred :(";
+  return errorMessage;
+};
+
+// base url
+axios.defaults.baseURL = "http://127.0.0.1:5000";
+// handle authorization
+axios.interceptors.request.use(handleAuthorizationHeader);
+// error handling
+axios.interceptors.response.use(null, handleErrors);
+
+const exportedProperties = {
+  get: axios.get,
+  post: axios.post,
+  put: axios.put,
+  delete: axios.delete,
+};
+export default exportedProperties;
