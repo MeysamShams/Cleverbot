60681b6d486a5720c0ac3f6f94ca19958617439c
Meysam Shamsebagi <meysam.shm@outlook.com>
2023-05-04 10:47:32 +0330

    feat: add https support

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,8 @@
+import {readFileSync} from 'fs'
+
+import {createServer as createHttpServer} from 'http'
+import {createServer as createHttpsServer} from 'https'
+
 import express,{Express} from 'express'
 import * as dotenv from 'dotenv'
 import cors from 'cors';
@@ -5,6 +10,8 @@ import routes from './routes'
 import { setResponseStatusCode } from './middlewares/setResponseStatusCode.middleware';
 
 dotenv.config();
+// ssl 
+
 
 const app:Express=express();
 
@@ -13,8 +20,23 @@ app.use(express.json())
 app.use(setResponseStatusCode)
 app.use(routes)
 
-app.listen(
-    parseInt(process.env.SERVER_PORT||"5000"),
+// http server
+const httpServer=createHttpServer(app)
+httpServer.listen(
+    parseInt(process.env.HTTP_PORT||"5000"),
     "0.0.0.0",
-    ()=>console.log(`Server is running on port ${process.env.SERVER_PORT}`)
+    ()=>console.log(`Http server is running on port ${process.env.HTTP_PORT}`)
 )
+
+// https server
+if(process.env.PRIVATE_KEY && process.env.CERTIFICATE){
+    const privateKey  = readFileSync(process.env.PRIVATE_KEY||"", 'utf8');
+    const certificate = readFileSync(process.env.CERTIFICATE||"", 'utf8');
+    const httpsServer=createHttpsServer({key:privateKey,cert:certificate},app)
+    httpsServer.listen(
+        parseInt(process.env.HTTPS_PORT||"600"),
+        "0.0.0.0",
+        ()=>console.log(`Https server is running on port ${process.env.HTTPS_PORT}`)
+    )
+    
+}
